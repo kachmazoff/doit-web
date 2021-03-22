2c5b9dbe38efa426e212d7896e7d1dfd4ca53522
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-22 17:55:43 +0400

    Update PageHeader (use bootstrap)

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./pageHeader.module.css";
 
 export const PageHeader = () => {
   return (
-    <div>
-      <span>doit</span>
-    </div>
+    <header className={styles.wrapper}>
+      <Container fluid="md">
+        <Row>
+          <Col xs={2}>
+            <span className={styles.logo}>doit</span>
+          </Col>
+          <Col>notification</Col>
+          <Col xs={3}>Profile info</Col>
+        </Row>
+      </Container>
+    </header>
   );
 };
