bd6a0d807272b3bf11186bf00c2a8a6b3241ede5
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-22 20:16:30 +0400

    Add transparent prop to Block component

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,14 +1,22 @@
 import React, { FC, useState } from "react";
 import styles from "./block.module.css";
 
+const getBlockStyle = (transparent: boolean) =>
+  transparent ? styles.block_transparent : styles.block;
+
 export const Block: FC<
-  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-> = ({ children, className, ...rest }) => {
-  const [classNames, setClassNames] = useState<string>(styles.block);
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > & { transparent?: boolean }
+> = ({ children, transparent, className, ...rest }) => {
+  const [classNames, setClassNames] = useState<string>(
+    getBlockStyle(!!transparent)
+  );
 
   React.useEffect(() => {
-    setClassNames((className || "") + " " + styles.block);
-  }, [className]);
+    setClassNames([className || "", getBlockStyle(!!transparent)].join(" "));
+  }, [className, transparent]);
 
   return (
     <div {...rest} className={classNames}>
