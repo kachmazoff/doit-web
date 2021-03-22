8f51ad10788d7d9af7be6770837b478376e7537e
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-22 19:17:02 +0400

    Handle className prop on Block component

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -1,10 +1,18 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from "./block.module.css";
 
 export const Block: FC<
   React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-> = ({ children, ...rest }) => (
-  <div {...rest} className={styles.block}>
-    {children}
-  </div>
-);
+> = ({ children, className, ...rest }) => {
+  const [classNames, setClassNames] = useState<string>(styles.block);
+
+  React.useEffect(() => {
+    setClassNames((className || "") + " " + styles.block);
+  }, [className]);
+
+  return (
+    <div {...rest} className={classNames}>
+      {children}
+    </div>
+  );
+};
