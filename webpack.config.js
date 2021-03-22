43028cda887878ee174301e7aba24dacf18e2fc8
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-22 09:43:53 +0400

    Install new deps and update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const { DefinePlugin } = require("webpack");
+const dotenv = require("dotenv");
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   mode: "development",
   entry: "./src/index.tsx",
   output: {
     filename: "bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -14,25 +24,45 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
     ],
   },
-  devServer: {
-    historyApiFallback: true,
-  },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".css"],
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   devServer: {
+    historyApiFallback: true,
     overlay: true,
     open: true,
-    hot: true
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new DefinePlugin(envKeys),
   ],
 };
