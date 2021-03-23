af17d04c103c556759a30f89f14a536e323222e1
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-23 23:49:48 +0400

    Sketch of app with existing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import { Block, Menu, PageHeader } from "./components";
+import { CreateChallengePage, HomePage, RegistrationPage } from "./pages";
 
-export const App = () => <h1>Hello world</h1>;
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <PageHeader />
+        <Container fluid="md">
+          <Row>
+            <Col xs={2}>
+              <Block transparent>
+                <Menu />
+              </Block>
+            </Col>
+            <Switch>
+              <Route path="/registration" exact component={RegistrationPage} />
+              <Route
+                path="/challenge_create"
+                exact
+                component={CreateChallengePage}
+              />
+              <Route path="/" exact component={HomePage} />
+            </Switch>
+          </Row>
+        </Container>
+      </div>
+    </BrowserRouter>
+  );
+};
