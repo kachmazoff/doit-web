98f8517d7deca9731420c91195c1235f85fecd42
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-23 23:47:44 +0400

    Update styles for FastActionsBlock and use Link

--- src/components/FastActionsBlock/FastActionsBlock.tsx
+++ src/components/FastActionsBlock/FastActionsBlock.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Block } from "../Block";
 import styles from "./fastActions.module.css";
 
@@ -7,8 +8,12 @@ export const FastActionsBlock = () => {
     <Block>
       <header className={styles.header}>Быстрые действия</header>
       <ul className={styles.list}>
-        <li>Создать челлендж</li>
-        <li>Добавить запись</li>
+        <li>
+          <Link to="/challenge_create" className={styles.action}>
+            Создать челлендж
+          </Link>
+        </li>
+        <li className={styles.action}>Добавить запись</li>
       </ul>
     </Block>
   );
