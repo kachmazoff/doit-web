d16fa8d8d3ec4b15312ff32da1a379dd48468d3c
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-23 21:25:55 +0400

    Create Button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { PageHeader } from "./PageHeader";
 export { Block } from "./Block";
 export { Menu } from "./Menu";
 export { FastActionsBlock } from "./FastActionsBlock";
+export { Button } from "./Button";
