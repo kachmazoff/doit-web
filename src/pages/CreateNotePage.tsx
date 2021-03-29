7353a377984abd1125978f682f675da28c7835ff
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-29 17:09:56 +0400

    Handle note creation

--- src/pages/CreateNotePage.tsx
+++ src/pages/CreateNotePage.tsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Col } from "react-bootstrap";
 import { NoteForm } from "@/forms";
+import { Participant } from "@/types";
 
-export const CreateNotePage = () => (
-  <>
-    <Col>
-      <NoteForm />
-    </Col>
-    <Col xs={3} className="hidden-sm-down" />
-  </>
-);
+export const CreateNotePage = () => {
+  const [participants, setParticipants] = useState<Participant[]>([]);
+
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    const endpoint = `/api/users/${user.username}/participants`;
+    const headers = {};
+    if (!!token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    axios.get(endpoint, { headers }).then((x) => {
+      setParticipants(x.data);
+      console.log(x.data);
+    });
+  }, []);
+
+  const onSubmit = React.useCallback((formData) => {
+    console.log(formData);
+
+    const token = localStorage.getItem("token");
+
+    const endpoint = `/api/participants/${formData.participant_id}/notes`;
+    const headers = {};
+    if (!!token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    axios.post(endpoint, formData, { headers }).then((x) => {
+      console.log(x.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Col>
+        <NoteForm participants={participants} onSubmit={onSubmit} />
+      </Col>
+      <Col xs={3} className="hidden-sm-down" />
+    </>
+  );
+};
