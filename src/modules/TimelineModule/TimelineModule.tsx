4a9683181891e98b66fab0fa6eda1752e88a8851
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-29 22:48:59 +0400

    Create TimelineModule

---
+++ src/modules/TimelineModule/TimelineModule.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import axios, { AxiosError } from "axios";
+import { Timeline, TimelineItemModel } from "@/components";
+
+const endpointConfig = {
+  common: "/",
+  own: "/own",
+  personalized: "/personalized",
+};
+
+const getHeaders = () => {
+  const token = localStorage.getItem("token");
+
+  const headers = {};
+  if (!!token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return headers;
+};
+
+type TimelineTypes = keyof typeof endpointConfig;
+
+const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+};
+
+const getTimeline = (type: TimelineTypes) => {
+  const endpoint = "/api/timeline" + endpointConfig[type];
+
+  const headers = getHeaders();
+
+  return axios.get(endpoint, { headers }).then((x) => x.data);
+};
+
+export const TimelineModule = ({ type }: { type: TimelineTypes }) => {
+  const [status, setStatus] = React.useState("loading");
+  const [timelineItems, setTimelineItems] = React.useState([]);
+
+  React.useEffect(() => {
+    setStatus("loading");
+
+    getTimeline(type)
+      .then((data) => {
+        setTimelineItems(data);
+        setStatus("success");
+      })
+      .catch((x: AxiosError) => {
+        console.error(x);
+        if (x.response.status === 401) {
+          logout();
+          setStatus("failed");
+        }
+      });
+  }, [type]);
+
+  return (
+    <div>
+      {status === "loading" && <p>Загрузка</p>}
+      {status === "failed" && <p>Ошибка</p>}
+
+      {status === "success" && (
+        <Timeline
+          items={timelineItems as TimelineItemModel[]}
+          keyField="index"
+        />
+      )}
+    </div>
+  );
+};
