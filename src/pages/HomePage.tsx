63f59541028421c8e735cbd446bafbffb2f4f9d0
kachmazoff <alek.kachmazov@yandex.ru>
2021-03-29 17:10:17 +0400

    Add data fetching for HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,96 @@
 import React from "react";
-import { Col } from "react-bootstrap";
-import { Timeline, FastActionsBlock, TimelineItemModel } from "@/components";
+import axios, { AxiosError } from "axios";
+import { Button, Col } from "react-bootstrap";
+import {
+  Timeline,
+  FastActionsBlock,
+  TimelineItemModel,
+  Block,
+} from "@/components";
+import { LoginFormMin } from "@/forms";
 
-const timelineMock = [
-  { id: "woegrvjip", type: "CREATE_CHALLENGE" },
-  { id: "woegrvjipeowuhijpfo", type: "ACCEPT_CHALLENGE" },
-  { id: "woegrvjipuiwhdjbo", type: "CREATE_CHALLENGE" },
-  { id: "woegrvjip1", type: "CREATE_CHALLENGE" },
-  { id: "woegrvjipeowuhijpfo1", type: "ACCEPT_CHALLENGE" },
-  { id: "woegrvjipuiwhdjbo1", type: "CREATE_CHALLENGE" },
-  { id: "woegrvjip2", type: "CREATE_CHALLENGE" },
-  { id: "woegrvjipeowuhijpfo2", type: "ACCEPT_CHALLENGE" },
-  { id: "woegrvjipuiwhdjbo2", type: "CREATE_CHALLENGE" },
-];
+const getHeaders = () => {
+  const token = localStorage.getItem("token");
 
-export const HomePage = () => (
-  <>
-    <Col>
-      <Timeline items={timelineMock as TimelineItemModel[]} />
-    </Col>
-    <Col xs={3}>
-      <FastActionsBlock />
-    </Col>
-  </>
-);
+  const headers = {};
+  if (!!token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return headers;
+};
+
+const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+};
+
+const getTimeline = () => {
+  const token = localStorage.getItem("token");
+  const common = "/api/timeline";
+  const personalized = "/api/timeline/personalized";
+  const own = "/api/timeline/own";
+  const headers = getHeaders();
+
+  return axios
+    .get(!!token ? personalized : common, { headers })
+    .then((x) => x.data)
+    .catch((x: AxiosError) => {
+      console.log(x.response.status);
+      if (x.response.status === 401) {
+        logout();
+        return getTimeline();
+      }
+    });
+};
+
+export const HomePage = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [timelineItems, setTimelineItems] = React.useState([]);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    getTimeline()
+      .then(setTimelineItems)
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const isLoggedIn = localStorage.getItem("user") !== null;
+
+  return (
+    <>
+      <Col>
+        {isLoading ? (
+          <p>Загрузка</p>
+        ) : (
+          <Timeline
+            items={timelineItems as TimelineItemModel[]}
+            keyField="index"
+          />
+        )}
+      </Col>
+      <Col xs={3}>
+        {!isLoggedIn ? (
+          <LoginFormMin
+            onSubmit={(data) => {
+              const { email, password } = data.target.elements;
+              const body = { email: email.value, password: password.value };
+
+              axios.post("/api/auth/login", body).then((x) => {
+                console.log(x.data);
+                localStorage.setItem("token", x.data.token);
+                localStorage.setItem("user", JSON.stringify(x.data.user));
+              });
+            }}
+          />
+        ) : (
+          <Block>
+            <Button onClick={logout}>Выйти</Button>
+          </Block>
+        )}
+        <FastActionsBlock />
+      </Col>
+    </>
+  );
+};
