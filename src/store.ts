e6783a626fc14929e3ce301a4a1ed7167c29b5b8
kachmazoff <alek.kachmazov@yandex.ru>
2021-04-01 19:43:57 +0400

    Create slice for auth module

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "@/modules/AuthModule";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  [authSlice.name]: authSlice.reducer,
+});
 
 export type RootState = ReturnType<typeof reducer>;
 
