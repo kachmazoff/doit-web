7074760050d9651c62566491329579c4e61ff3a0
kachmazoff <alek.kachmazov@yandex.ru>
2021-04-01 16:38:21 +0400

    Small update for own participants

--- src/pages/OwnParticipationsPage.tsx
+++ src/pages/OwnParticipationsPage.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { Col } from "react-bootstrap";
 import { getUserParticipations } from "@/api";
+import { dateFormat } from "@/utils";
+import { Block } from "@/components";
 
 export const OwnParticipationsPage = () => {
   const [participations, setParticipations] = React.useState([]);
   React.useEffect(() => {
-    getUserParticipations("kachmazoff").then(setParticipations);
+    getUserParticipations("kachmazoff").then((list) => {
+      console.log(list);
+      setParticipations(list);
+    });
   }, []);
   return (
-    <div>
-      {participations.map((x) => (
-        <p>{x.challenge.title}</p>
-      ))}
-    </div>
+    <>
+      <Col>
+        <Block transparent>
+          <h2>Дневники</h2>
+        </Block>
+        {participations.map((x) => (
+          <Block key={x.id}>
+            <p>Дата начала {dateFormat(x.created)}</p>
+            <p>{x.challenge.title}</p>
+          </Block>
+        ))}
+      </Col>
+      <Col xs={3}></Col>
+    </>
   );
 };
