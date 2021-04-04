b032005741cf8be1397b76cedf941ee400037aa1
kachmazoff <alek.kachmazov@yandex.ru>
2021-04-04 13:26:47 +0400

    Update OwnParticipationsPage

--- src/pages/OwnParticipationsPage.tsx
+++ src/pages/OwnParticipationsPage.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Col } from "react-bootstrap";
 import { getUserParticipations } from "@/api";
 import { Block, ParticipationsList } from "@/components";
+import { getAuthState } from "@/modules/AuthModule";
 
 export const OwnParticipationsPage = () => {
+  const authState = useSelector(getAuthState);
   const [participations, setParticipations] = React.useState([]);
   React.useEffect(() => {
-    getUserParticipations("kachmazoff").then((list) => {
-      console.log(list);
-      setParticipations(list);
-    });
-  }, []);
+    const { userData } = authState;
+    if (!!userData && userData.username)
+      getUserParticipations(userData.username).then((list) => {
+        setParticipations(list);
+      });
+  }, [authState.userData]);
   return (
     <>
       <Col>
